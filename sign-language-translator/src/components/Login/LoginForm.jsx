734bc21b2cd9417707d4f12e4f6b29c8b57eb8bf
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-01-25 14:50:15 +0100

    LoginForm Tailwind css

--- sign-language-translator/src/components/Login/LoginForm.jsx
+++ sign-language-translator/src/components/Login/LoginForm.jsx
@@ -57,15 +57,20 @@ const LoginForm = () => {
 
     return (
         <>
-        <h2 className="text-3xl font-bold underline">What's your name?</h2>
         <form onSubmit={ handleSubmit(onSubmit) }>
-            <fieldset>
-                <label htmlFor='username'>Username: </label>
-                <input type="text" placeholder='John Doe'{...register("username", usernameConfig)} />
-                { errorMessage }
-            </fieldset>
-            <button type="submit" disabled={loading}>Continue</button>
-
+            <div class="flex justify-center">
+                <div class="mb-3 xl:w-96">
+                    <label htmlFor='username' class="form-label inline-block mb-2 text-gray-700">Username</label>
+                    <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid 
+                    border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder='John Doe'{...register("username", usernameConfig)} />
+                </div>
+            </div>
+            { errorMessage }
+            <div class="flex space-x-2 justify-center">
+                <button type="submit" disabled={loading} data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md 
+                hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                    Continue</button>
+            </div>
             {loading && <p>Logging in...</p>}
             {apiError && <p>{apiError}</p>}
         </form>
