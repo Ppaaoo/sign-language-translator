ec49ff1d2915e89d7a06a7c59f26b6e79a737d1c
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-01-25 15:52:26 +0100

    tailwind css again

--- sign-language-translator/src/components/Login/LoginForm.jsx
+++ sign-language-translator/src/components/Login/LoginForm.jsx
@@ -60,7 +60,8 @@ const LoginForm = () => {
         <form onSubmit={ handleSubmit(onSubmit) }>
             <div class="flex justify-center">
                 <div class="mb-3 xl:w-96">
-                    <label htmlFor='username' class="form-label inline-block mb-2 text-gray-700">Username</label>
+                    <label htmlFor='username' class="form-label inline-block my-2.5 text-gray-700">
+                        Username</label>
                     <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid 
                     border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder='John Doe'{...register("username", usernameConfig)} />
                 </div>
@@ -71,7 +72,12 @@ const LoginForm = () => {
                 hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                     Continue</button>
             </div>
-            {loading && <p>Logging in...</p>}
+            {loading && 
+                <div class="flex justify-center items-center">
+                    <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+                        <span class="visually-hidden"></span>
+                    </div>
+                </div>}
             {apiError && <p>{apiError}</p>}
         </form>
         </>
--- sign-language-translator/src/components/NavBar/NavBar.jsx
+++ sign-language-translator/src/components/NavBar/NavBar.jsx
@@ -5,21 +5,21 @@ const NavBar = () => {
 
     const {user} = useUser()
     return (
-        <nav>
-            <ul style={{listStyle:'none'}}>
-                <li>Lost in Translation</li>
-            </ul>
+        <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+                <p class="text-xl text-white pr-2 font-semibold">Lost in Translation</p>
 
-            { user !== null &&
-            <ul>
-                <li>
-                    <NavLink to="/profile">Profile</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/translate">Translate</NavLink>
-                </li>
-            </ul>
-            }
+                { user !== null &&
+                <div class="flex items-center">
+                    <div class="py-4 text-white opacity-60 hover:opacity-80 focus:opacity-80 p-2">
+                            <NavLink to="/profile">Profile</NavLink>
+                    </div>
+                    <div class="py-4 text-white opacity-60 hover:opacity-80 focus:opacity-80 p-2">
+                            <NavLink to="/translate">Translate</NavLink>
+                    </div>
+                </div>
+                }
+            </div>
         </nav>
     )
 }
